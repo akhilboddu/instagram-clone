f077a4258592a98fbcfb6efbc7c931d9166a142b
[lissa2004] <[mathikgek21@gmail.com]>
2024-04-08 17:44:30 +0200

    added modal

--- app.js
+++ app.js
@@ -1,89 +1,44 @@
 
-const firebaseAuthContainer = document.querySelector("#firebase-auth-container");
-const mainContainer = document.querySelector("#main-container");
-const searchInput = document.querySelector("#search-input");
-const searchBtn = document.querySelector("#search-btn");
-const postsDiv = document.querySelector(".posts");
+  const firebaseAuthContainer = document.querySelector("#firebase-auth-container");
+  const mainContainer = document.querySelector(".main-container");
+  const searchInput = document.querySelector("#search-input");
+  const searchBtn = document.querySelector("#search-btn");
+  const postsDiv = document.querySelector(".posts");
+  const usernameInput = document.querySelector("#username-input");
+  const imageLinkInput = document.querySelector("#image-link-input");
+  const captionInput = document.querySelector("#caption-input");
+  const createPostBtn = document.querySelector("#create-post-btn");
+  const editPostBtn = document.querySelector("#edit-post-btn");
+  const logoutBtn = document.querySelector("#logout-btn");
+  editPostBtn.style.display = "none";
+  const editBtn = document.querySelector("#edit-btn");
+  // const ui = new firebaseui.auth.AuthUI(auth);
+  const showCreateModal = document.querySelector("#show-create-modal");
+  
+//   const modalBody = SelectorEngine.findOne('.modal-body', document.querySelector('#exampleModal'));
+// if (modalBody) {
+//   modalBody.scrollTop = 0;
+// }
 
-const usernameInput = document.querySelector("#username");
-const imageLinkInput = document.querySelector("#imagelink");
-const captionInput = document.querySelector("#caption");
-const createPostBtn = document.querySelector("#create-post-btn");
-const editPostBtn = document.querySelector("#edit-post-btn");
-const logoutBtn = document.querySelector("#logout-btn");
-   editPostBtn.style.display = "none";
+// Get the modal element
+const modalElement = document.getElementById('exampleModal');
+const modal = new bootstrap.Modal(modalElement, {
+  backdrop: 'static', 
+  keyboard: false
+});
 
-const editBtn = document.querySelector("#edit-btn");
-
-const modal = new bootstrap.Modal(document.getElementById("modal"));
-const showCreateModal = document.querySelector("#show-create-modal");
-
-const ui = new firebaseui.auth.AuthUI(auth);
-
-// Rest of the code (redirectToAuth, redirectToApp, handleLogout, and auth.onAuthStateChanged) goes here
-
-const redirectToAuth = () => {
-  mainContainer.style.display = "none";
-  firebaseAuthContainer.style.display = "block";
-
-  ui.start("#firebase-auth-container", {
-    callbacks: {
-      signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        console.log("authResult", authResult.user.uid);
-        // this.userId = authResult.user.uid;
-        // this.$authUserText.innerHTML = user.displayName;
-        redirectToApp();
-      },
-    },
-    signInOptions: [
-      firebaseui.auth.EmailAuthProvider.PROVIDER_ID,
-      firebaseui.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    // Other config options...
-  });
-};
-
-const redirectToApp = () => {
-  mainContainer.style.display = "block";
-  firebaseAuthContainer.style.display = "none";
-};
-
-const handleLogout = () => {
-  auth
-    .signOut()
-    .then(() => {
-      console.log("USER SIGNED OUT");
-      redirectToAuth();
-    })
-    .catch((error) => {
-      console.log("ERROR OCCURRED", error);
-    });
-};
-
- onAuthStateChanged((user) => {
-    if (user) {
-      console.log(user.uid);
-      // this.userId = user.uid;
-      // this.$authUserText.innerHTML = user.displayName;
-      redirectToApp();
-    } else {
-      console.log("not loggedin");
-      redirectToAuth();
-    }
-  });
-
-// Event Listeners
+  // Event Listeners
 searchBtn.addEventListener("click", () => {
   console.log(searchInput.value);
   searchInput.style.background = "red";
 });
+
+
 createPostBtn.addEventListener("click", () => {
   console.log("create post btn clicked");
   createPost(imageLinkInput.value, captionInput.value, usernameInput.value);
 });
+
 showCreateModal.addEventListener("click", () => {
   isEditMode = false;
   createPostBtn.style.display = "block";
@@ -93,6 +48,7 @@ showCreateModal.addEventListener("click", () => {
   captionInput.value = "";
   modal.show();
 });
+
 editPostBtn.addEventListener("click", () => {
   console.log("edit post btn clicked");
   updatePost(postToEditId, imageLinkInput.value, captionInput.value);
@@ -102,6 +58,59 @@ logoutBtn.addEventListener("click", () => {
   handleLogout();
 });
 
+
+  const redirectToAuth = () => {
+    mainContainer.style.display = "none";
+    firebaseAuthContainer.style.display = "block";
+
+    ui.start("#firebase-auth-container", {
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          console.log("authResult", authResult.user.uid);
+          // this.userId = authResult.user.uid;
+          // this.$authUserText.innerHTML = user.displayName;
+          redirectToApp();
+        },
+      },
+      signInOptions: [
+        firebaseui.auth.EmailAuthProvider.PROVIDER_ID,
+        firebaseui.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+      // Other config options...
+    });
+  };
+
+  const redirectToApp = () => {
+    mainContainer.style.display = "block";
+    firebaseAuthContainer.style.display = "none";
+  };
+
+  const handleLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        console.log("USER SIGNED OUT");
+        redirectToAuth();
+      })
+      .catch((error) => {
+        console.log("ERROR OCCURRED", error);
+      });
+  };
+
+  onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user.uid);
+        // this.userId = user.uid;
+        // this.$authUserText.innerHTML = user.displayName;
+        redirectToApp();
+      } else {
+        console.log("not loggedin");
+        redirectToAuth();
+      }
+    });
 
 var feed = [];
 var isEditMode = false;
@@ -197,7 +206,6 @@ const deletePost = (id) => {
     }
   });
   feed = updatedFeed;
-  // outputFeed(feed);
 };
 
 const outputPostStatus = (post) => {
